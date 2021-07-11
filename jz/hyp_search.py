916879bdc14ad3361126618cd6022a2c86b5432e
Mateo <mmahaut@ensc.fr>
2021-07-11 14:59:29 +0200

    minor change : double quotations

--- jz/hyp_search.py
+++ jz/hyp_search.py
@@ -13,7 +13,7 @@ def run_batch(path):
     for name in grid:
         experiments.append(grid[name])
         param_names.append(name)
-
+    count = 0
     for combination in itertools.product(*experiments):
         txt_params = ""
         job_name = "egg"
@@ -23,10 +23,12 @@ def run_batch(path):
                 job_name += f"_{param_names[i][:2]}{element}"
         logdir = f"/gpfsscratch/rech/imi/ude64um/simple_egg_exp/{job_name}"
         txt_params += f" --tensorboard_dir={logdir}"
-        if not os.path.exists(logdir):
-            os.makedirs(logdir)
+        # if not os.path.exists(logdir):
+        #     os.makedirs(logdir)
         print(txt_params)
         # run_exp(job_name, "first_exp.py", txt_params, gpu=True, time="1:00:00")
+        count += 1
+    print(f"submitted {count} jobs")
 
 
 run_batch("jz/hyper_params.json")
