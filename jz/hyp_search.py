cd069b5d9bdc2fc52d3a527a0955f719c5c45742
Mateo <mmahaut@ensc.fr>
2021-07-11 15:01:08 +0200

    minor change : tested, ready for execution

--- jz/hyp_search.py
+++ jz/hyp_search.py
@@ -23,10 +23,10 @@ def run_batch(path):
                 job_name += f"_{param_names[i][:2]}{element}"
         logdir = f"/gpfsscratch/rech/imi/ude64um/simple_egg_exp/{job_name}"
         txt_params += f" --tensorboard_dir={logdir}"
-        # if not os.path.exists(logdir):
-        #     os.makedirs(logdir)
-        print(txt_params)
-        # run_exp(job_name, "first_exp.py", txt_params, gpu=True, time="1:00:00")
+        if not os.path.exists(logdir):
+            os.makedirs(logdir)
+        # print(txt_params)
+        run_exp(job_name, "first_exp.py", txt_params, gpu=True, time="1:00:00")
         count += 1
     print(f"submitted {count} jobs")
 
